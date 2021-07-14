587ed043f9a0765bfb38de1a817eb9cc408ad666
TTatchell <tim.tatchell24@gmail.com>
2021-07-14 14:35:34 +1000

    upload

--- lib/song.rb
+++ lib/song.rb
@@ -24,9 +24,8 @@ class Song
   def self.new_by_filename(string)
     split = string.split(" - ")
     song_instance = Song.new(split[1])
-    artist_name = split[0]
+    song_instance.artist_name=(split[0])
     
-    # song_instance.artist = artist_name
     song_instance
   end
 
@@ -34,7 +33,6 @@ class Song
     artist_instance = Artist.find_or_create_by_name(name)
     artist_instance.add_song(self)
     @artist = artist_instance
-    binding.pry
     
 
   end
